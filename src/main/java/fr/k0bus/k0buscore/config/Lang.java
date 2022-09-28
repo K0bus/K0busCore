e57aa5f405d7f2ca72b56fffc560d14b88767c95
K0bus <contact@k0bus.fr>
2022-09-28 22:33:52 +0200

    Add Material, Entities, Enchants Language

--- src/main/java/fr/k0bus/k0buscore/config/Lang.java
+++ src/main/java/fr/k0bus/k0buscore/config/Lang.java
@@ -1,14 +1,22 @@
 package fr.k0bus.k0buscore.config;
 
 import fr.k0bus.k0buscore.utils.StringUtils;
+import org.bukkit.Material;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.EntityType;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.InputStreamReader;
 
 public class Lang extends Configuration{
+
+    private boolean material = false;
+    private boolean entities = false;
+    private boolean enchants = false;
+
     public Lang(String lang, JavaPlugin instance) {
         super(lang + ".yml", instance, "lang");
         FileConfiguration defaultConfig = null;
@@ -28,8 +36,63 @@ public class Lang extends Configuration{
                 if(!configuration.contains(defaultConfig.getCurrentPath() + "." +key))
                     configuration.set(defaultConfig.getCurrentPath() + "." + key, defaultConfig.get(key));
             }
-
     }
+
+    public void loadMaterial()
+    {
+        for (Material m: Material.values()) {
+            if(!isString("minecraft.material." + m.name()))
+                this.set("minecraft.material." + m.name(), StringUtils.proper(m.name()));
+        }
+    }
+    public void loadEntities()
+    {
+        for (EntityType e: EntityType.values()) {
+            if(!isString("minecraft.entities." + e.name()))
+                this.set("minecraft.entities." + e.name(), StringUtils.proper(e.name()));
+        }
+    }
+    public void loadEnchant()
+    {
+        for (Enchantment e: Enchantment.values()) {
+            if(!isString("minecraft.enchants." + e.getName()))
+                this.set("minecraft.enchants." + e.getName(), StringUtils.proper(e.getName()));
+        }
+    }
+
+    public String getMaterialName(Material m)
+    {
+        if(isString("minecraft.material." + m.name()))
+            return getString("minecraft.material." + m.name());
+        return StringUtils.proper(m.name());
+    }
+
+    public String getEntitiesName(EntityType e)
+    {
+        if(isString("minecraft.entities." + e.name()))
+            return getString("minecraft.entities." + e.name());
+        return StringUtils.proper(e.name());
+    }
+    public String getEntitiesName(Enchantment e)
+    {
+        if(isString("minecraft.enchants." + e.getName()))
+            return getString("minecraft.enchants." + e.getName());
+        return StringUtils.proper(e.getName());
+    }
+
+    public boolean hasEnchants()
+    {
+        return enchants;
+    }
+    public boolean hasMaterials()
+    {
+        return material;
+    }
+    public boolean hasEntities()
+    {
+        return entities;
+    }
+
     public void checkSection(ConfigurationSection section)
     {
         for(String key: section.getKeys(false))
